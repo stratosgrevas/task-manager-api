49d2a7d4dba7f2a00a9004ecb7959d44dd737ef8
Allan Grevas <stratosgrevas@gmail.com>
2019-04-01 14:06:02 -0300

    Config the routes file for the api - 2

--- config/routes.rb
+++ config/routes.rb
@@ -6,11 +6,15 @@ Rails.application.routes.draw do
   # api. e nesse caso é preciso criar essa rota em namespace.
   # Poderia ser uma pasta chamada ADMIN. E dentro dela teria controllers que seriam
   # para controlar coisas do amin.
-  namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api' }, path: "/" do
+  namespace :api, defaults: { format: :json } do
 
   end
 end
 
+############
+#
+# CONFIGURAÇÃO PARA SUBDOMINIO NO SERVIDOR (LOCAL NÃO FUNCIONA!!!):
+# 
 # api = www.site.com/api/tasks
 # tasks = nome do controller
 # constraints (restrições)
@@ -19,3 +23,13 @@ end
 # como o path = "/", então o subdominio = api.site.com/tasks
 # 
 # trabalhar com subdominio é melhor para trabalhar com os DNS do servidor.
+# 
+# OBS: api.localhost:3000 (não funciona!!!)
+# 
+# essa alteração, fica num arquivo que só pode ser alterado como administrador!!!
+# 
+# namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api' }, path: "/" do
+# end
+# 
+# 
+############# 

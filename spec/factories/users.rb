35be1f0dd433756a66fb1d22757d06e73598679b
Allan Grevas <stratosgrevas@gmail.com>
2019-04-02 16:11:57 -0300

    Adding initial tests to user model

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,4 +1,7 @@
 FactoryGirl.define do 
 	factory :user do
+		email "adriano@teste.com"
+		password "teste"
+		password_confirmation "teste"
 	end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  	#pending "add some examples to (or delete) #{__FILE__}"
+  
+  	# antes de cada teste, será executado o before!
+  	before { @user = FactoryGirl.build(:user) }
+
+  	# ####################
+  	# Começando os testes
+	# ####################
+ 
+	# esste teste, espera que a instancia @user resposda pelo atributo e-mail 
+	it { expect(@user).to respond_to(:email) }
+	it { expect(@user).to respond_to(:name) }
+	it { expect(@user).to respond_to(:password) }
+	it { expect(@user).to respond_to(:password_confirmation) }
+	it { expect(@user).to respond_to(:created_at) }
+	it { expect(@user).to respond_to(:updated_at) }
+	it { expect(@user).to respond_to(:id) }
+	# objeto válido?
+	it { expect(@user).to be_valid }
+
 end
+
+
+#<User id: nil, email: "aniyah@klein.co", created_at: nil, updated_at: nil>

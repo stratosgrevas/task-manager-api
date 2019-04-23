85b0edd8b610b8dbbdcabe0a2cd77f7723686501
Allan Grevas <stratosgrevas@gmail.com>
2019-04-23 15:51:04 -0300

    changing double quotes to simple quotes

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,24 @@
 require "api_version_constraint"
 
 Rails.application.routes.draw do
-  
+
   # devise_for :users, cria algumas rotas. Muito útil para criar
   # rotas de usuários.
   # devise_for :users (pode dar conflito com as rotas dos testes)
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # BOAS PRÁTICAS
+  # '' => aspsas simples, para string simples
+  # "" => aspas duplas, para quando tem concatenação de strings e variáveis.
   
   # namespace é para agrupar e organizar melhor os nossos controllers
   # por exemplo: todos os controllers, que fazem parte da api, eu vou agrupar dentro da pasta
   # api. e nesse caso é preciso criar essa rota em namespace.
   # Poderia ser uma pasta chamada ADMIN. E dentro dela teria controllers que seriam
   # para controlar coisas do amin.
-  namespace :api, defaults: { format: :json }, path: "/" do
-  	namespace :v1, path: "/", constraints: ApiVersionConstraint.new(version: 1, default: true) do
+  namespace :api, defaults: { format: :json }, path: '/' do
+  	namespace :v1, path: '/', constraints: ApiVersionConstraint.new(version: 1, default: true) do
       #resources :tasks
   		resources :users, only: [:show]
   	end
--- spec/requests/api/v1/users_spec.rb
+++ spec/requests/api/v1/users_spec.rb
@@ -7,29 +7,29 @@ RSpec.describe 'Users API', type: :request do
 
 	let(:user_id) { user.id }
 
-	# before { host "api.dominio.dev" }
+	# before { host 'api.dominio.dev' }
 
-	describe "GET users/:id" do # action show
+	describe 'GET users/:id' do # action show
 		before do
-			headers = { "Accept" => "application/vnd.taskmanager.v1" }
+			headers = { 'Accept' => 'application/vnd.taskmanager.v1' }
 			get "/users/#{user_id}", params: {}, headers: headers
 		end
 
-		context "when the user exists" do
-			it "returns the user" do
+		context 'when the user exists' do
+			it 'returns the user' do
 				user_response = JSON.parse(response.body)
-				expect(user_response["id"]).to eq(user_id)
+				expect(user_response['id']).to eq(user_id)
 			end
 
-			it "returns status code 200" do
+			it 'returns status code 200' do
 				expect(response).to have_http_status(200)
 			end
 		end
 
-		context "when the user does not exists" do
+		context 'when the user does not exists' do
 			let(:user_id) { 10000 }
 
-			it "returns status code 404" do
+			it 'returns status code 404' do
 				expect(response).to have_http_status(404)
 			end
 		end

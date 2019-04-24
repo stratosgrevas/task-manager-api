ec1f9cef35c127b599a8c310053bc08f8fd1a511
Allan Grevas <stratosgrevas@gmail.com>
2019-04-24 16:43:14 -0300

    adding destroy action to the users controller with tests

--- config/routes.rb
+++ config/routes.rb
@@ -20,7 +20,7 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json }, path: '/' do
   	namespace :v1, path: '/', constraints: ApiVersionConstraint.new(version: 1, default: true) do
       #resources :tasks
-  		resources :users, only: [:show, :create, :update]
+  		resources :users, only: [:show, :create, :update, :destroy]
   	end
   end
 end

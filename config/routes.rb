412b85f5b353c364d66d8902f97a0b5748f4f99d
Allan Grevas <stratosgrevas@gmail.com>
2019-04-24 16:34:04 -0300

    adding update action to the users controller with tests

--- config/routes.rb
+++ config/routes.rb
@@ -20,7 +20,7 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json }, path: '/' do
   	namespace :v1, path: '/', constraints: ApiVersionConstraint.new(version: 1, default: true) do
       #resources :tasks
-  		resources :users, only: [:show, :create]
+  		resources :users, only: [:show, :create, :update]
   	end
   end
 end

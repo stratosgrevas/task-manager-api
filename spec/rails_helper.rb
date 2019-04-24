e0bd17c44c01006559f2040a6c54b7113e1aa22e
Allan Grevas <stratosgrevas@gmail.com>
2019-04-24 17:46:36 -0300

    Refactoring the tests

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -21,7 +21,7 @@ require 'database_cleaner'
 # directory. Alternatively, in the individual `*_spec.rb` files, manually
 # require only the support files necessary.
 #
-# Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 
 # Checks for pending migration and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove this line.
@@ -39,7 +39,10 @@ RSpec.configure do |config|
     end
   end
 
+  # carregando o FactoryGirl nos testes
   config.include FactoryGirl::Syntax::Methods
+  # carregando o RequestSpecHelper para os testes do tipo REQUEST!
+  config.include RequestSpecHelper, type: :request
 
 
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
--- spec/requests/api/v1/users_spec.rb
+++ spec/requests/api/v1/users_spec.rb
@@ -7,18 +7,26 @@ RSpec.describe 'Users API', type: :request do
 
 	let(:user_id) { user.id }
 
+	let(:headers) do
+		{
+			'Accept' => 'application/vnd.taskmanager.v1',
+			'Content-Type' => Mime[:json].to_s
+		}
+	end
+
 	# before { host 'api.dominio.dev' }
 
 	describe 'GET users/:id' do # action show
 		before do
-			headers = { 'Accept' => 'application/vnd.taskmanager.v1' }
 			get "/users/#{user_id}", params: {}, headers: headers
 		end
 
 		context 'when the user exists' do
 			it 'returns the user' do
-				user_response = JSON.parse(response.body, symbolize_names: true)
-				expect(user_response[:id]).to eq(user_id)
+				# Exemplo não refatorado, ou seja, sem helper
+				# user_response = JSON.parse(response.body, symbolize_names: true)
+				# expect(user_response[:id]).to eq(user_id)
+				expect(json_body[:id]).to eq(user_id)
 			end
 
 			it 'returns status code 200' do
@@ -37,8 +45,7 @@ RSpec.describe 'Users API', type: :request do
 
 	describe 'POST /users' do
 		before do
-			headers = { "Accept" => "application/vnd.taskmanager.v1" }
-      		post '/users', params: { user: user_params }, headers: headers
+      		post '/users', params: { user: user_params }.to_json, headers: headers
 		end
 
 		context 'when the request param are valid' do
@@ -49,8 +56,7 @@ RSpec.describe 'Users API', type: :request do
 			end
 
 			it 'returns the json data for the created user' do
-				user_response = JSON.parse(response.body, symbolize_names: true)
-				expect(user_response[:email]).to eq(user_params[:email])
+				expect(json_body[:email]).to eq(user_params[:email])
 			end
 		end
 
@@ -62,16 +68,14 @@ RSpec.describe 'Users API', type: :request do
 			end
 
 			it 'returns the json data for the errors' do
-				user_response = JSON.parse(response.body, symbolize_names: true)
-				expect(user_response).to have_key(:errors)
+				expect(json_body).to have_key(:errors)
 			end
 		end
 	end
 
 	describe 'PUT /users/:id' do
 		before do
-			headers = { "Accept" => "application/vnd.taskmanager.v1" }
-      		put "/users/#{user_id}", params: { user: user_params }, headers: headers
+      		put "/users/#{user_id}", params: { user: user_params }.to_json, headers: headers
 		end
 
 		context 'when the request param are valid' do
@@ -82,8 +86,7 @@ RSpec.describe 'Users API', type: :request do
 			end
 
 			it 'returns the json data for the updated user' do
-				user_response = JSON.parse(response.body, symbolize_names: true)
-				expect(user_response[:email]).to eq(user_params[:email])
+				expect(json_body[:email]).to eq(user_params[:email])
 			end
 		end
 
@@ -95,15 +98,13 @@ RSpec.describe 'Users API', type: :request do
 			end
 
 			it 'returns the json data for the errors' do
-				user_response = JSON.parse(response.body, symbolize_names: true)
-				expect(user_response).to have_key(:errors)
+				expect(json_body).to have_key(:errors)
 			end
 		end
 	end
 
 	describe 'DELETE /users/:id' do
 		before do
-			headers = { "Accept" => "application/vnd.taskmanager.v1" }
       		delete "/users/#{user_id}", params: {}, headers: headers
 		end
 

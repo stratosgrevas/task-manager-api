22c96e5e52e21cd37261a6aac044893a35466c6b
Allan Grevas <stratosgrevas@gmail.com>
2019-04-25 18:49:56 -0300

    Updating User#info

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -12,10 +12,15 @@ RSpec.describe User, type: :model do
   	# Quando um describe tiver um '#+alguma palavra', será um teste de instância
   	# ou teste de função que vai estar no seu model.
   	describe '#info' do
-  		it 'returns email and created_at' do
+  		it 'returns email and created_at and a Token' do
   			user.save!
 
-  			expect(user.info).to eq("#{user.email} - #{user.created_at}")
+  			# criando um MOCK
+  			allow(Devise).to receive(:friendly_token).and_return('abc123xyzTOKEN')
+
+  			# PXab_qssWAEG3nQ12svm - token de exemplo gerado pelo Devise.friendly_token
+  			# comando rodado no console rails c
+  			expect(user.info).to eq("#{user.email} - #{user.created_at} - Token: abc123xyzTOKEN")
   		end
   	end
 end

71629067a6f1dfee8d46dda134c64e2c2bb6d8ab
Allan Grevas <stratosgrevas@gmail.com>
2019-05-13 11:37:38 -0300

    Adding destroy action to the tasks controller with tests

--- app/controllers/api/v1/tasks_controller.rb
+++ app/controllers/api/v1/tasks_controller.rb
@@ -34,6 +34,12 @@ class Api::V1::TasksController < ApplicationController
 		end
 	end
 
+	def destroy
+		task = current_user.tasks.find(params[:id])
+		task.destroy
+		head 204
+	end
+
 	private
 
 	def task_params

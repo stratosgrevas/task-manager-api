1a0c8230f686daf06b53f77233ba5cb3501c935a
Allan Grevas <stratosgrevas@gmail.com>
2019-05-15 16:54:16 -0300

    Adding Ransack for tasks

--- app/controllers/api/v2/tasks_controller.rb
+++ app/controllers/api/v2/tasks_controller.rb
@@ -2,7 +2,7 @@ class Api::V2::TasksController < ApplicationController
 	before_action :authenticate_with_token!
 
 	def index
-		tasks = current_user.tasks
+		tasks = current_user.tasks.ransack(params[:q]).result
 		render json: tasks, status: 200 # versão do padrão JSON.API
 		# render json: { tasks: tasks }, status: 200 # versão fora do padrao do JSON.API
 	end

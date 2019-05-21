a0b3cfac0076a3b9155e958600682d52ff34b41d
Allan Grevas <stratosgrevas@gmail.com>
2019-05-21 16:54:10 -0300

    allow to send name as atribute for users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,14 @@
 class ApplicationController < ActionController::API
+
+	before_action :configure_permitted_parameters, if: :devise_controller?
+
+	protected
+
+	def configure_permitted_parameters
+		attrs = [:name]
+		devise_parameter_sanitizer.permit(:sign_up, keys: attrs)
+		devise_parameter_sanitizer.permit(:account_update, keys: attrs)
+	end
     
 	# Exemplo de um current user
 	# def current_user

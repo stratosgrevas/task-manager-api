83918d7a59a303016137b9f2b798346bb633f22e
Allan Grevas <stratosgrevas@gmail.com>
2019-05-21 16:16:17 -0300

    Updating the Sessions API

--- spec/requests/api/v2/sessions_spec.rb
+++ spec/requests/api/v2/sessions_spec.rb
@@ -5,18 +5,24 @@ RSpec.describe 'Sessions API', type: :request do
 	
 	let(:user) { create(:user) }
 
+	let!(:auth_data) { user.create_new_auth_token }
+
 	# Accept = versão da api
 	let(:headers) do
 		{
 			'Accept' => 'application/vnd.taskmanager.v2',
-			'Content-Type' => Mime[:json].to_s
+			'Content-Type' => Mime[:json].to_s,
+			'access-token' => auth_data['access-token'],
+			'uid' => auth_data['uid'],
+			'client' => auth_data['client']
 		}
 	end
 
 	# criar a sessão do usuário
-	describe 'POST /sessions' do
+	# login com o devise token auth
+	describe 'POST /auth/sign_in' do
 		before do
-			post '/sessions', params: { session: credentials }.to_json, headers: headers
+			post '/auth/sign_in', params: credentials.to_json, headers: headers
 		end
 
 		context 'when the credentials are correct' do
@@ -28,9 +34,10 @@ RSpec.describe 'Sessions API', type: :request do
 				expect(response).to have_http_status(200)
 			end
 
-			it 'returns the json data for the user with auth token' do
-				user.reload
-				expect(json_body[:data][:attributes][:'auth-token']).to eq(user.auth_token)
+			it 'returns the authentication data in the headers' do
+				expect(response.headers).to have_key('access-token')
+				expect(response.headers).to have_key('uid')
+				expect(response.headers).to have_key('client')				
 			end
 		end
 
@@ -49,19 +56,29 @@ RSpec.describe 'Sessions API', type: :request do
 	end
 
 	# destruir a sessão do usuário
-	describe 'DELETE /sessions/:id' do
+	# logout com o devise token auth
+	describe 'DELETE /auth/sign_out' do
 		let(:auth_token) { user.auth_token }
 
 		before do
-			delete "/sessions/#{auth_token}", params: {}, headers: headers
+			delete '/auth/sign_out', params: {}, headers: headers
 		end
 
-		it 'returns status code 204' do
-			expect(response).to have_http_status(204)
+		it 'returns status code 200' do
+			expect(response).to have_http_status(200)
 		end
 
+		# logout com o devise token auth
 		it 'changes the user auth token' do
-			expect( User.find_by(auth_token: auth_token) ).to be_nil
+			# recarrega o usuário para pegar os dados atualizados do banco de dados.
+			user.reload
+
+			# valid_token? é uma função do devise token auth
+			# que pode ser localizada dentro do DOC dessa gem
+			expect( user.valid_token?(auth_data['access-token'], auth_data['client']) ).to eq(false)
+
+			# outra maneira de executar o mesmo teste:
+			# expect( user ).not_to be_valid_token(auth_data['access-token'], auth_data['client'])
 		end
 	end
 end
